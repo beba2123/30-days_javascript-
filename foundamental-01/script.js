0c83eb628672ae263d5a917ca2241a5a535ac05f
beba2123 <antenehbeba@gmail.com>
2023-05-11 13:59:52 +0300

    second commit

--- foundamental-01/script.js
+++ foundamental-01/script.js
@@ -1,3 +1,56 @@
 let js = "amazing";
-if(js=="amazing")
-    alert ("JavaScript is amazing...");
+// if(js=="amazing")   
+// alert ("JavaScript is amazing...");
+// const firstName = "Anteneh";
+// console.log(firstName)
+// console.log(js)
+// let JavaScriptIsFun = "amaazing";
+// let boolean = true;
+
+// if(JavaScriptIsFun != "amazing")
+//   console.log(!boolean)
+//   else 
+//     console.log(boolean)
+
+// console.log(typeof boolean)
+
+// let lastName;
+//   console.log(typeof lastName) //undefine.(empty value)
+
+// lastName = "Alem";
+//  console.log(lastName)
+//  console.log( typeof lastName) //(string)
+
+
+//  const firstName = " Anteneh ";
+//  const lastName = " Alem ";
+
+//  console.log(firstName + lastName)
+ 
+
+//  let x = 25;
+//  x *= 10;
+//  console.log(x);
+//  const age = 11;
+//  if(age >= 18)
+//  console.log(" you are permitted to drink ")
+//  else if(age < 18)
+//  console.log(" you are not allowed to drink..mitu ")
+ 
+ 
+//  const bebaAge = 23;
+//  const bettAge = 20;
+
+//  const averageAge = (bebaAge + bettAge) / 2;
+//  console.log(averageAge)
+
+const MarkWeight = 65;
+const MarkHeight = 1.70;
+const johnWeight = 90;
+const johnHeight = 1.75;
+
+const BMImark = (MarkWeight)/ MarkHeight ** MarkHeight;
+const BMIjohn = (johnWeight)/ johnHeight ** johnHeight;
+
+console.log(BMIjohn)
+console.log(BMImark)
