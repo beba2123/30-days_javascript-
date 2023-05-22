7cf6686c1673dcb295b956e508e8989fbec57164
beba2123 <antenehbeba@gmail.com>
2023-05-22 12:34:42 +0300

    updated project commit

---
+++ 05-Guess-My-Number/starter/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "Correct Number.!"; //changing the text-content using the dom manupilation.
+//console.log(document.querySelector(".message").textContent); //then print out the changing text
+
+document.querySelector(".number").textContent = 4;
+document.querySelector(".score").textContent = 20;
