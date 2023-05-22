2cc0ad60e242f73062b65133b73bacac0573a508
beba2123 <antenehbeba@gmail.com>
2023-05-22 14:16:54 +0300

    updated project commit

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,8 +1,24 @@
 "use strict";
 
-console.log(document.querySelector(".message").textContent);
-document.querySelector(".message").textContent = "Correct Number.!"; //changing the text-content using the dom manupilation.
-//console.log(document.querySelector(".message").textContent); //then print out the changing text
+// console.log(document.querySelector(".message").textContent);
+// document.querySelector(".message").textContent = "Correct Number.!"; //changing the text-content using the dom manupilation.
+// //console.log(document.querySelector(".message").textContent); //then print out the changing text
 
-document.querySelector(".number").textContent = 4;
-document.querySelector(".score").textContent = 20;
+// document.querySelector(".number").textContent = 4;
+// document.querySelector(".score").textContent = 20;
+
+// document.querySelector(".guess").value = 23;
+// console.log(document.querySelector(".guess").value);
+const GuessNumber = Math.trunc(Math.random()*20)+1;
+console.log(GuessNumber);
+
+
+const Eventhandler = function () {
+  const guess = document.querySelector(".guess").value;
+  console.log(guess);
+  if (!guess) {
+    document.querySelector(".message").textContent = " NO Number ... ";
+  }
+};
+
+document.querySelector(".check").addEventListener("click", Eventhandler);
