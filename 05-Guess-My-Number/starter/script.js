5804931d71fffcd6491032a7164253796cf93bb6
beba2123 <antenehbeba@gmail.com>
2023-05-23 09:55:49 +0300

    udated commit on Guess

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,13 +12,18 @@
 let GuessNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
+const displayMessage = function(message){
+  document.querySelector('.message').textContent = message;
+}
 const Eventhandler = function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
   if (!guess) {
-    document.querySelector('.message').textContent = ' NO Number ... ';
+    // document.querySelector('.message').textContent = ' NO Number ... ';
+      displayMessage('No NUMBER..');
   } else if (guess === GuessNumber) {
-    document.querySelector('.message').textContent = 'Correct Number.!';
+    // document.querySelector('.message').textContent = 'Correct Number.!';
+    displayMessage('CORRECT Number..');
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = GuessNumber; 
@@ -26,34 +31,48 @@ const Eventhandler = function () {
         highscore = score;
         document.querySelector('.highscore').textContent = highscore;
     }
-  } else if (guess > GuessNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = ' Too  Higher';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = ' you lost.. ';
-      document.querySelector('.score').textContent = 0;
-    }
-  } else if (guess < GuessNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = ' Too LOW';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = ' you lost.. ';
-      document.querySelector('.score').textContent = 0;
-    }
   }
+    else if(guess !== GuessNumber){
+      if(score > 1){
+        // document.querySelector('.message').textContent = guess > GuessNumber ? 'Too Higher..' : 'Too Lower..'
+        displayMessage( guess > GuessNumber ? 'Too Higher..' : 'Too Lower..');
+        score--;
+        document.querySelector('.score').textContent = score;
+      }else if(score < 1){
+        // document.querySelector('.message').textcontent = 'You Lost ..';
+        displayMessage('You Lost..');
+        document.querySelector('.score').textContent = 0;
+      }
+    }
+  //  else if (guess > GuessNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = ' Too  Higher';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = ' you lost.. ';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  // } else if (guess < GuessNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = ' Too LOW';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = ' you lost.. ';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  // }
 };
 
-document.querySelector('.check').addEventListener('click', Eventhandler);
+document.querySelector('.check').addEventListener('click', Eventhandler());
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   document.querySelector('.score').textContent = score;
   GuessNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.message').textContent = ' Start guessing... ';
+  // document.querySelector('.message').textContent = ' Start guessing... ';
+  displayMessage('Start Guessing..!');
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
