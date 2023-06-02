1226e29deb563efacf756519a35bfa52457b9ed3
beba2123 <antenehbeba@gmail.com>
2023-06-02 14:23:30 +0300

    intial commit

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
    //for select the element and store them in a variable...
+ const player0  = document.querySelector('.player--0');
+ const player1  = document.querySelector('.player--1');
  const current_Score = document.querySelector('.current-score');
  const score1 = document.querySelector('#score--0');
  const score2 = document.getElementById('score--1');
@@ -12,7 +14,9 @@
 score1.textContent = 0;
 score2.textContent = 0;
 dicepic.classList.add('hidden');
+let scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 //rolling dice function..
 
 btnRoll.addEventListener('click', function(){
@@ -26,9 +30,17 @@ btnRoll.addEventListener('click', function(){
     //store the generated random number..
     if(diceNo !== 1){ //then we are going to or stored the number..
         currentScore += diceNo;
-        current_Score.textContent = currentScore;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore; //this one is for making it more  dynamic..
+        // current_Score.textContent = currentScore;
     }
-    else{  // switch the player if it is one.. 
-  
+    else if(diceNo === 1){  // switch the player if it is one.. 
+        document.getElementById(`current--${activePlayer}`).textContent  = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;  //switching the players
+        currentScore =0;
+        player0.classList.toggle('player--active'); //which is for changing the ui btn the player 1 and player 0.
+        player1.classList.toggle('player--active'); 
     }
+})
+btnHold.addEventListener('click', function(){
+    //store a function
 })
