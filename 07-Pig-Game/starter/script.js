a7ff18960ea497852a0c9cdbe720327c64fb5823
beba2123 <antenehbeba@gmail.com>
2023-06-02 16:36:58 +0300

    final commit for pig-game commit

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,21 +3,43 @@
    //for select the element and store them in a variable...
  const player0  = document.querySelector('.player--0');
  const player1  = document.querySelector('.player--1');
- const current_Score = document.querySelector('.current-score');
- const score1 = document.querySelector('#score--0');
+ const current0 = document.getElementById('current--0');
+ const current1 = document.getElementById('current--1');
+ const score1 = document.getElementById('score--0');
  const score2 = document.getElementById('score--1');
  const dicepic = document.querySelector('.dice');
  const btnNew = document.querySelector('.btn--new');
  const btnRoll = document.querySelector('.btn--roll');
  const btnHold = document.querySelector('.btn--hold');
  //for automatically convert the element to 0...
-score1.textContent = 0;
-score2.textContent = 0;
-dicepic.classList.add('hidden');
-let scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+
+let scores;
+let currentScore ;
+let activePlayer;
+let playing ;
+const initializeFunction = function() {
+     scores = [0, 0];
+     currentScore = 0;
+     activePlayer = 0;
+     playing = true;
+     
+     score1.textContent = 0;
+     score2.textContent = 0;
+     current0.textContent = 0;
+     current1.textContent = 0;
+     dicepic.classList.add('hidden');
+
+
+    
+     
+     document.querySelector('.player--1').classList.remove('player--winner');
+     document.querySelector('.player--1').classList.add('player--active');
+     document.querySelector('.player--0').classList.remove('player--active');
+     document.querySelector('.player--0').classList.remove('player--winner');
+     
+}
+initializeFunction()
+
 //switching function..
 const switchPlayer = function(){
     document.getElementById(`current--${activePlayer}`).textContent  = 0;
@@ -67,3 +89,4 @@ btnHold.addEventListener('click', function(){
 
    }
 })
+btnNew.addEventListener('click', initializeFunction)
