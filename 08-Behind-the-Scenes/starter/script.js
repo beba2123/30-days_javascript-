da03271438053c691222f20cf535101f480e3522
beba2123 <Antenehbeba@gmail.com>
2023-06-17 09:53:06 -0700

    intial commit

---
+++ 08-Behind-the-Scenes/starter/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+ function CalcAge(birthYear){
+    const age  = Number(2037 - birthYear);
+    console.log(firstName);
+    console.log(age)
+    return age;
+ }
+
+ const firstName = 'Jonas';
+ CalcAge(1991);
