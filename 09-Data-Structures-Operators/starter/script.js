0410a095680ea9bdb21e1bf388a27099e7cd4bfe
beba2123 <Antenehbeba@gmail.com>
2023-06-27 02:02:24 +0300

    updated commit

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -5,33 +5,33 @@
 //   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // // Data needed for first part of the section
-// const restaurant = {
-//   name: 'Classico Italiano',
-//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
-//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-//   order: function(starterIndex, mainIndex){
-//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-//   },
-//   openingHours: {
-//     thu: {
-//       open: 12,
-//       close: 22,
-//     },
-//     fri: {
-//       open: 11,
-//       close: 23,
-//     },
-//     sat: {
-//       open: 0, // Open 24 hours
-//       close: 24,
-//     },
-//   },
-//   pizzaIngre: function(ingre1, ingre2, ingre3){
-//     return ingre1 + ingre2 + ingre3;
-//   }
-// };
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function(starterIndex, mainIndex){
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+  pizzaIngre: function(ingre1, ingre2, ingre3){
+    return ingre1 + ingre2 + ingre3;
+  }
+};
 
 // // const [Italian, , Vegetarian, ...otherFood] = [...restaurant.categories, ...restaurant.starterMenu];
 // // console.log(otherFood);
@@ -217,3 +217,60 @@ const[players1, players2] = game.players;
 // }
 // printGoals('Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels');
 // printGoals(...game.scored);
+
+ const Menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+//  for(const item of Menu){
+//  console.log(item);
+//  }
+// for(const [index, element] of Menu.entries()){
+//   console.log(`${index + 1} : ${element}`);
+// }
+
+// const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+// for(const day of days){
+//    console.log(day);
+//   const open = restaurant.openingHours[day]?.open ?? 'no hours' ;  // using the null coelesion for saturday becouse  zero undefined when  
+//   console.log(`on this day we open at ${open}`);
+// }
+
+//we can also use it for a funtion to check wether that method also exist or not.
+// const beba1 ={
+//   name: 'beba',
+//   hello: function(){
+//     console.log('hi people ..');
+//   }
+// }
+//  beba1.hello?.() ?? 'there is no methods with  this name.';
+ //when we want to loop over the keys which  is  located inside the object.
+
+// const  proper = Object.keys(restaurant.openingHours)
+// console.log(proper)
+//  let properStr = `we are open ${proper.length} days per week: `;
+//  for(const day of proper){
+//   properStr += ` ${day}, `
+//  }
+//  console.log(properStr)
+ 
+
+//  const entries = Object.entries(restaurant.openingHours);
+//  console.log(entries);
+//  for(const [keys, {open, close}] of  entries){
+//   console.log(`on ${keys} we are open at ${open} and close at ${close}`);
+//  }
+//  const entries1 = Object.entries(game.scored);
+//  for(const [keys, value] of game.scored.entries()){  //
+//   console.log(`Goal ${keys + 1}: ${value}`)
+//  }
+  let sum =0;
+ for(const odd of Object.values(game.odds)){
+     sum += odd;
+    // console.log(odd)
+ }
+ console.log(sum/3); 
+ for(const [team, value] of Object.entries(game.odds)){
+   const output = team  === 'x' ? 'draw' : `victory ${game[team]}`;
+   console.log(`odds of ${output}:${value}`); 
+ }
+ const mySet = new Set(['anteneh', 'alem', 'resoto', 'alem']);
+
+ console.log(mySet);
